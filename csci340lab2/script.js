0323fe261ebf83919e04d456614619984b717c9b
Gabby <gnaples19@gmail.com>
2021-09-10 13:22:19 -0500

    Trying 'get'

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -2,8 +2,8 @@
 $(document).ready(function() {
 $('.button').click(function() {
   $.ajax({
-    dataType: "jsonpCallback",
-    jsonpCallback: "parseQuote",
+    dataType: "jsonp",
+    jsonpCallback: "get",
     url: "https://breaking-bad-quotes.herokuapp.com/v1/quotes",
     success: function(results) {
       $('.qoute').text(results["quoteText"]);
