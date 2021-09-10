27f59c13d0e0f01a4796f5b56d51b2f3d2d4bbe2
Gabby <gnaples19@gmail.com>
2021-09-10 13:43:43 -0500

    This might work

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 $('.button').click(function() {
   $.ajax({
     dataType: "json",
-    //jsonpCallback: "parseQuote",
-    url: "https://breaking-bad-quotes.herokuapp.com/v1/quotes",
+    jsonpCallback: "parseQuote",
+    url: "https://breaking-bad-quotes.herokuapp.com/v1/quotes?method=getQuote&format=jsonp&lang=en&jsonp=parseQuote",
     success: function(results) {
       $('.qoute').text(results["qoutes"]);
     },
