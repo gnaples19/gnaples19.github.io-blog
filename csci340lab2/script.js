ec927fd1d78e501c5265704b386dca6870071f17
Gabby <gnaples19@gmail.com>
2021-09-10 13:23:34 -0500

    'quote'

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -6,7 +6,7 @@ $('.button').click(function() {
     jsonpCallback: "get",
     url: "https://breaking-bad-quotes.herokuapp.com/v1/quotes",
     success: function(results) {
-      $('.qoute').text(results["quoteText"]);
+      $('.qoute').text(results["qoute"]);
     },
     error: function(xhr,status,error) {
       console.log(error);
