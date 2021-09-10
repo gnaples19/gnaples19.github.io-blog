a63ea07bab48dc5c4ac6d518320c9dfc0192d0a8
Gabby <gnaples19@gmail.com>
2021-09-10 09:41:52 -0500

    Going to look for console errors now

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -6,7 +6,7 @@ $('.button').click(function() {
     jsonpCallback: "parseQuote",
     url: "https://breaking-bad-quotes.herokuapp.com/v1/quotes",
     success: function(results) {
-      $('.qoute').text(results["quote"]);
+      $('.qoute').text(results["quoteText"]);
     },
     error: function(xhr,status,error) {
       console.log(error);
