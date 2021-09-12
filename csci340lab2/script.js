03b7a1e650d983cc768941b194052ee261eda5db
Gabby <gnaples19@gmail.com>
2021-09-12 00:12:54 -0500

    Cosmetic changes

--- csci340lab2/script.js
+++ csci340lab2/script.js
@@ -1,17 +1,30 @@
-
 $(document).ready(function() {
-  console.console.log("testing");
-$('.button').click(function() {
-  $.ajax({
-    dataType: "json",
-    jsonpCallback: "parseQuote",
-    url: "https://breaking-bad-quotes.herokuapp.com/v1/quotes?method=getQuote&format=jsonp&lang=en&jsonp=parseQuote",
-    success: function(results) {
-      $('.qoute').text(results["quoteText"]);
-    },
-    error: function(xhr,status,error) {
-      console.log(error);
-    }
+  $('#catButton').click(function() {
+    $.ajax({
+      dataType: "json",
+      url: "https://api.thecatapi.com/v1/images/search?format=json",
+      success: function(results) {
+        console.log(results["0"]);
+          console.log("testing success");
+          $('#cat').attr("src", results[0]["url"]);
+      },
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
+    });
+  });
+  $('#catButton').click(function() {
+    $.ajax({
+      dataType: "json",
+      url: "https://api.thedogapi.com/v1/images/search?format=json",
+      success: function(results) {
+        console.log(results["0"]);
+          console.log("dog success");
+          $('#dog').attr("src", results[0]["url"]);
+      },
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
   });
 });
 });
