93e9bac70ec6ea2f8dafd81750f322dbf144d33d
Alivia <AllenAA@Hendrix.edu>
2021-11-29 13:50:55 -0600

    repasted wix api code in place of previous code

--- API_Testing/script.js
+++ API_Testing/script.js
@@ -1,15 +1,55 @@
 $(document).ready(function() {
   $('#fetchButton').click(function() {
-    $.ajax({
-      dataType: "json",
-      url: "https://api.thecatapi.com/v1/images/search?format=json",
-      success: function(results) {
-        console.log(results["0"]);
-          console.log("testing success");
-          $('#cat').attr("src", results[0]["url"]);
-      },
-      error: function(xhr,status,error) {
-        console.log(error);
+
+import wixStoresBackend from 'wix-stores-backend';
+import wixData from 'wix-data';
+
+export function getOrdersLink(orderIds) {
+  return wixStoresBackend.getOrdersLink(orderIds);
+}
+
+// Get IDs of the last 10 paid orders
+export function getPaidOrderIds() {
+  let options = {
+    "suppressAuth": true
+  };
+
+  return wixData.query('Stores/Orders')
+    .eq("paymentStatus", 'PAID')
+    .descending('_dateCreated')
+    .limit(10)
+    .find(options)
+    .then((results) => {
+      if (results.items.length > 0) {
+        // Order IDs found
+        const orderIds = results.items.map(order => order._id)
+        return orderIds;
+      } else {
+        return "No orders found";
       }
+    })
+    .catch((error) => {
+      return error;
+    })
+}
+
+import { getOrdersLink, getPaidOrderIds } from 'backend/orders';
+
+getPaidOrderIds()
+  .then((orderIds) => {
+    getOrdersLink(orderIds)
+      .then((link) => {
+        // Orders PDF link retrieved
+        const orderPdfUrl = link;
+      })
+      .catch((error) => {
+        // Orders PDF link not retrieved
+        console.error(error)
+      })
+  })
+  .catch((error) => {
+    // Orders not retrieved from backend
+    console.error(error)
+  });
     });
   });
