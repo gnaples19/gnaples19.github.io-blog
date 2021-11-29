7a785d0e1ec7ceab452ab5704ce16d654f307203
Gabby <gnaples19@gmail.com>
2021-11-29 13:36:50 -0600

    Initial Commits, mostly copy from lab 2

---
+++ API_Testing/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('#fetchButton').click(function() {
+    $.ajax({
+      dataType: "json",
+      url: "https://api.thecatapi.com/v1/images/search?format=json",
+      success: function(results) {
+        console.log(results["0"]);
+          console.log("testing success");
+          $('#cat').attr("src", results[0]["url"]);
+      },
+      error: function(xhr,status,error) {
+        console.log(error);
+      }
+    });
+  });
