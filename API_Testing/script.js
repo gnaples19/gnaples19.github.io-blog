4c6c5786ffe2b012b9bc3ad68cd82600458fdbd9
Alivia <AllenAA@Hendrix.edu>
2021-11-29 13:48:15 -0600

    pasted get order pdf code from wix

--- API_Testing/script.js
+++ API_Testing/script.js
@@ -1,5 +1,7 @@
 import wixStores from 'wix-stores';
 import wixStoresBackend from 'wix-stores-backend';
+import wixData from 'wix-data';
+
 
 $(document).ready(function() {
   $('#fetchButton').click(function() {
@@ -14,5 +16,56 @@ $(document).ready(function() {
       error: function(xhr,status,error) {
         console.log(error);
       }
+      /*****************************
+ * Backend code - orders.jsw *
+ *****************************/
+
+export function getOrdersLink(orderIds) {
+  return wixStoresBackend.getOrdersLink(orderIds);
+}
+
+// Get IDs of the last 10 paid orders
+export function getPaidOrderIds() {
+  let options = {
+    "suppressAuth": true
+  };
+
+  return wixData.query('Stores/Orders')
+    .eq("paymentStatus", 'PAID')
+    .descending('_dateCreated')
+    .limit(10)
+    .find(options)
+    .then((results) => {
+      if (results.items.length > 0) {
+        // Order IDs found
+        const orderIds = results.items.map(order => order._id)
+        return orderIds;
+      } else {
+        return "No orders found";
+      }
+    })
+    .catch((error) => {
+      return error;
+    })
+}
+
+import { getOrdersLink, getPaidOrderIds } from 'backend/orders';
+
+getPaidOrderIds()
+  .then((orderIds) => {
+    getOrdersLink(orderIds)
+      .then((link) => {
+        // Orders PDF link retrieved
+        const orderPdfUrl = link;
+      })
+      .catch((error) => {
+        // Orders PDF link not retrieved
+        console.error(error)
+      })
+  })
+  .catch((error) => {
+    // Orders not retrieved from backend
+    console.error(error)
+  });
     });
   });
